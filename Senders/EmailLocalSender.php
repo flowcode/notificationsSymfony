07302533ccd7f79718d2b21ebe3fa60d6aab2769
Julián Scialabba <julian.scialabba@gmail.com>
2018-01-17 10:31:05 -0300

    Add attachment emailLocalSender

--- Senders/EmailLocalSender.php
+++ Senders/EmailLocalSender.php
@@ -23,27 +23,35 @@ class EmailLocalSender implements EmailSenderInterface
     public function send($toEmail, $toName, $fromEmail, $fromName, $subject, $body, $isHTML = false, $attachmentPath = null)
     {
         $message = Swift_Message::newInstance()
-                ->setSubject($subject)
-                ->setFrom(array($fromEmail => $fromName))
-                ->setTo(array($toEmail => $toName));
+            ->setSubject($subject)
+            ->setFrom(array($fromEmail => $fromName))
+            ->setTo(array($toEmail => $toName));
         if ($isHTML) {
             $message->setBody($body, 'text/html');
         } else {
             $message->setBody($body, 'text/plain');
         }
+        if($attachmentPath){
+           $message->attach(\Swift_Attachment::fromPath($attachmentPath));
+        }
 
-        $this->container->get("mailer")->send($message);
+        try {
+            $this->container->get("mailer")->send($message);
+        }
+        catch (\Swift_TransportException $e) {
+            var_dump($e->getMessage());
+        }
         return new EmailSenderResponse(true, EmailSenderResponse::status_sent);
     }
 
     public function sendTemplate($toEmail, $toName, $fromEmail, $fromName, $subject, $templateName, $templateVars = array(), $attachmentPath = null)
     {
-        
+
     }
 
     public function sendTemplateMultipleRecipients($recipients, $fromEmail, $fromName, $subject, $templateName, $templateGlobalVars = array())
     {
-        
+
     }
 
 }
